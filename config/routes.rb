a29b21fa915adc5a7b637bf54f7bc7abf03561fa
Thomas Conklin <tconklin63@gmail.com>
2012-06-28 17:14:03 -0700

    Linked In Registration

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ FirstApp::Application.routes.draw do
   match '/signup',  to: 'users#new'
   match '/signin',  to: 'sessions#new'
   match '/signout', to: 'sessions#destroy', via: :delete
+  match '/linkedin', to: "static_pages#linkedin"
   
   resources :microposts
 
